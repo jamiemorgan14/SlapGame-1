4081fa8e72c4b20901fcf0c5b53881766f1e5bd4
Jamie <jmorgan3475@gmail.com>
2019-02-09 11:29:23 -0700

    3 buttons

--- app/app.js
+++ app/app.js
@@ -2,11 +2,24 @@ let health = 100;
 
 function slap() {
   health--;
-  update()
+  update();
 };
+
+function punch() {
+  health -= 5;
+  update();
+};
+
+function kick() {
+  health -= 10;
+  update();
+}
 
 function update() {
   document.getElementById('health').innerText = health.toString();
-}
+};
+
+
+
 
 update()
