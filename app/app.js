ecb87ba5ca69eb6a120b688d1c3ceb77aa209df7
Jamie <jmorgan3475@gmail.com>
2019-02-10 21:34:35 -0700

    Semi-finished

--- app/app.js
+++ app/app.js
@@ -1,19 +1,3 @@
-let characters = {
-  austin: {
-    name: 'Stone Cold',
-    health: 100,
-    hits: 0,
-    img: 'https://shop.wwe.com/on/demandware.static/-/Sites/default/dwd8f17634/images/slot/landing/superstar-landing/Superstar-Category_Superstar_562x408_stoneCold.png'
-
-  },
-  rock: {
-    name: 'The Rock',
-    health: 100,
-    hits: 0,
-    img: 'https://wwfoldschool.com/wp-content/uploads/2017/03/The-Rock.jpg'
-  }
-};
-
 let items = {
   brass: {
     name: 'Brass knuckles',
@@ -32,41 +16,130 @@ let items = {
   }
 }
 
-function hit() {
-  characters.austin.hits++;
+let characters = {
+  austin: {
+    name: 'Stone Cold',
+    health: 100,
+    hits: 0,
+    img: 'https://shop.wwe.com/on/demandware.static/-/Sites/default/dwd8f17634/images/slot/landing/superstar-landing/Superstar-Category_Superstar_562x408_stoneCold.png',
+    items: []
+  }
+};
+
+function reset() {
+  characters.austin.health = 100;
+  characters.austin.hits = 0;
+  characters.austin.items = [];
+  update();
+};
+
+function showMods() {
+  if (characters.austin.items.length > 0) {
+    for (let i = 0; i < characters.austin.items.length; i++) {
+      const mod = characters.austin.items[i];
+      document.getElementById('equipped-item').innerText = mod.description;
+    }
+  } else {
+    document.getElementById('equipped-item').innerText = 'No Items Equipped';
+  }
 }
 
-function slap() {
-  if (characters.austin.health > 0) {
-    characters.austin.health--;
+function addMods() {
+  let total = 0;
+  for (let i = 0; i < characters.austin.items.length; i++) {
+    total += characters.austin.items[i].modifier
+  };
+  return total;
+};
+
+function giveBrass() {
+  if (characters.austin.items.length <= 0) {
+    characters.austin.items.push(items.brass);
+  } else {
+    characters.austin.items.splice(0, 1, items.brass)
   }
+  addMods();
+  showMods();
+};
+
+function giveChair() {
+  if (characters.austin.items.length <= 0) {
+    characters.austin.items.push(items.chair);
+  } else {
+    characters.austin.items.splice(0, 1, items.chair);
+  }
+  addMods();
+  showMods();
+};
+
+function giveBat() {
+  if (characters.austin.items.length <= 0) {
+    characters.austin.items.push(items.bat);
+  } else {
+    characters.austin.items.splice(0, 1, items.bat)
+  }
+  addMods()
+  showMods();
+};
+
+function stayPositive() {
+  if (characters.austin.health < 0) {
+    characters.austin.health = 0;
+  }
+};
+
+function hit() {
+  characters.austin.hits++;
+};
+
+function slap() {
+  if (characters.austin.items.length > 0) {
+    characters.austin.health -= 1 * addMods();
+  } else {
+    characters.austin.health -= 1
+  }
+  stayPositive();
   hit();
   update();
 };
 
 function punch() {
-  if (characters.austin.health > 4) {
+  if (characters.austin.items.length > 0) {
+    characters.austin.health -= 5 * addMods();
+  } else {
     characters.austin.health -= 5;
   }
+  stayPositive();
   hit();
   update();
 };
 
 function kick() {
-  if (characters.austin.health > 9) {
-    characters.austin.health -= 10;
+  if (characters.austin.items.length > 0) {
+    characters.austin.health -= 10 * addMods();
+  } else {
+    characters.austin.health -= 10
   }
+  stayPositive();
   hit();
   update();
-}
+};
+
+function declareWinner() {
+  if (characters.austin.health == 0) {
+    document.getElementById('winner').innerText = `Stone Cold Steve Austin is down for the count!`
+  }
+};
 
 function update() {
   document.getElementById('health').innerText = characters.austin.health.toString();
   document.getElementById('hits').innerText = characters.austin.hits.toString();
   document.getElementById('name').innerText = characters.austin.name;
+  declareWinner();
+  showMods();
 };
 
 
 
+update();
 
-update()
